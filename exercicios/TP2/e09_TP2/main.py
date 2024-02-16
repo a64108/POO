37f8adb88edcd612359784a94b6dbcc447f23513
ANDRE FILIPE CORREIA VIEIRA <a64108@ualg.pt>
2024-02-16 16:17:58 +0000

    novo display

--- exercicios/TP2/e09_TP2/main.py
+++ exercicios/TP2/e09_TP2/main.py
@@ -1,8 +1,37 @@
-#!/usr/bin/env python3
-"""
-@date Feb 15 2024
-@authors: pcardoso, j-a-martins
-"""
+# ========================================================================
+# UALG - POO 
+# Aluno - 64108
+# Exercicio - 9
+# ========================================================================
+
+# Implemente as classes apresentadas no diagrama UML da Figura 1.
+
+#   Utilize o Visual Studio Code ou outro IDE avançado.
+
+#   Este diagrama foi escrito segundo as convenções do UML, por isso são necessárias algumas adaptações para a implementação em Python:
+
+#       Os nomes de atributos e métodos devem ser escritos no formato PEP8 – ou seja – em minúsculas, com as palavras separadas por _ (e.g., print_owner()).
+
+#       Os getters e setters dos atributos devem ser implementados como properties, através de decoradores (em vez de funções get e set).
+
+#           Quando se usam properties para encapsular atributos de classes, estes devem ser declarados como privados, ou seja, com um prefixo __ (e.g., self.__forename).
+
+#   Crie um programa que permita, de modo interativo: listar, inserir, remover, e editar carros de uma lista (veja o exemplo em baixo).
+
+#       Crie também uma opção para gravar essa lista num ficheiro (e.g., veja o módulo pickle).
+
+
+# ========================================================================
+
+# MAIN
+
+# Tem de se mudar as variaveis
+# printOwner() --> print_owner() 
+
+# Tudo o que for "get" out "set" vão ser implementadas a partir de decoradores
+
+# propriedades têm de ser privadas em com __nome
+
 
 from car import Car
 from person import Person
@@ -18,23 +47,25 @@ list_of_cars = []
 
 def main():
     while True:
-        print("Menu")
-        print("P1 - New Person")
-        print("P2 - List Persons")
+        print("========================================================================")
+        print("|                                MENU                                  |")
+        print("========================================================================")
+        print("|                                                                      |")
+        print("|        P1 - New Person                     P2 - List Persons         |")
+        print("|                                                                      |")
+        print("|        E1 - New Engine                     E2 - List Engines         |")
+        print("|                                                                      |")
+        print("|        C1 - New Color                      C2 - List Colors          |")
+        print("|                                                                      |")
+        print("|        V1 - New Car                        V2 - List Cars            |")
+        print("|                                                                      |")
+        print("========================================================================")
+        print("|                                                                      |")
+        print("|        S - Save lists                      L - Load lists            |")
+        print("|                                                                      |")
+        print("========================================================================")
 
-        print("E1 - New Engine")
-        print("E2 - List Engines")
-
-        print("C1 - New Color")
-        print("C2 - List Colors")
-
-        print("V1 - New Car")
-        print("V2 - List Cars")
-
-        print("S - Save lists")
-        print("L - Load lists")
-
-        op = input("Op? ").lower()
+        op = input("Opção? ").lower()
 
         match op:
             case "p1":
