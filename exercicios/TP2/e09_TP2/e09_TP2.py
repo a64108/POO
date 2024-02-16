f0b020989e6163bbce9d060a6465702d84e6ee9a
ANDRE FILIPE CORREIA VIEIRA <a64108@ualg.pt>
2024-02-16 15:47:05 +0000

    adicionada informaçao para o exercicio 9

--- exercicios/TP2/e09_TP2/e09_TP2.py
+++ exercicios/TP2/e09_TP2/e09_TP2.py
@@ -1,1 +1,34 @@
+# ========================================================================
+# UALG - POO 
+# Aluno - 64108
+# Exercicio - 9
+# ========================================================================
+
+# Implemente as classes apresentadas no diagrama UML da Figura 1.
+
+#   Utilize o Visual Studio Code ou outro IDE avançado.
+
+#   Este diagrama foi escrito segundo as convenções do UML, por isso são necessárias algumas adaptações para a implementação em Python:
+
+#       Os nomes de atributos e métodos devem ser escritos no formato PEP8 – ou seja – em minúsculas, com as palavras separadas por _ (e.g., print_owner()).
+
+#       Os getters e setters dos atributos devem ser implementados como properties, através de decoradores (em vez de funções get e set).
+
+#           Quando se usam properties para encapsular atributos de classes, estes devem ser declarados como privados, ou seja, com um prefixo __ (e.g., self.__forename).
+
+#   Crie um programa que permita, de modo interativo: listar, inserir, remover, e editar carros de uma lista (veja o exemplo em baixo).
+
+#       Crie também uma opção para gravar essa lista num ficheiro (e.g., veja o módulo pickle).
+
+
+# ========================================================================
+
 # MAIN
+
+# Tem de se mudar as variaveis
+# printOwner() --> print_owner() 
+
+# Tudo o que for "get" out "set" vão ser implementadas a partir de decoradores
+
+# propriedades têm de ser privadas em com __nome
+
